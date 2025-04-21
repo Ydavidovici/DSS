4080c049ef5c0f2b8a25539b9714121fc4fadc5b
Ydavidovici <ydavidovici@gmail.com>
2025-04-21 19:58:17 -0400

    initial commit

---
+++ apps/davidovici-frontend/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/*", (_, res) => {
+    res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Frontend running on http://localhost:${PORT}`);
+});
