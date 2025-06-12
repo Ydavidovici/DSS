ae0da8e70fb4a90f979e133f4fd7b37c0861aaca
Ydavidovici <ydavidovici@gmail.com>
2025-06-12 15:38:27 -0400

    removed tailwind

---
+++ apps/ds-frontend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+// serve all files under public/
+app.use(express.static(join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+    // if no static file matches, serve index.html
+    res.sendFile(join(__dirname, "public/pages/index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("🚀 Listening on http://localhost:3000");
+});
